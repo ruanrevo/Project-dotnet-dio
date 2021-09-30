6248cf09dfbe1579d95770d24f1b466be1979815
Ruan Lucas <mvk.ruan@gmail.com>
2021-09-30 10:05:05 -0300

    Git Inicial

---
+++ DIO.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Series>
+
+    {
+    private List<Series> listaSerie = new List<Series>();
+        public void Atualiza(int id, Series objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Series objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Series> lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Series RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
